8345190e3e7e62cf69d32cc0be6692c81c548716
Stian Thaulow <stian@thaulow.me>
2019-10-13 09:22:36 +0200

    Updates

---
+++ precache-manifest.bdc675f4ba81b0b0c75f8ff14b3a4312.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d57f4729d8f8710571f424d563a66a",
+    "url": "/max-orkester/index.html"
+  },
+  {
+    "revision": "015f7b32ce60420f09aa",
+    "url": "/max-orkester/static/css/main.0f7d688d.chunk.css"
+  },
+  {
+    "revision": "c59c9419936f3a6dca32",
+    "url": "/max-orkester/static/js/2.d4956fbc.chunk.js"
+  },
+  {
+    "revision": "015f7b32ce60420f09aa",
+    "url": "/max-orkester/static/js/main.721707d5.chunk.js"
+  },
+  {
+    "revision": "8bd9327e74029fcad466",
+    "url": "/max-orkester/static/js/runtime-main.b53ba1cd.js"
+  }
+]);
